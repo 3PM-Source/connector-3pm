65ac96c72fadb0aab5433069ecaf43a78b06ecef
Asjad88 <asjad.amin@3pm.nz>
2021-07-26 16:22:20 +1200

    login page progress

--- 3pm-connect-front-end/src/components/Master Container.tsx
+++ 3pm-connect-front-end/src/components/Master Container.tsx
@@ -3,16 +3,19 @@ import "../style/style.css";
 
 class MasterContainer extends Component {
     state = {
-
+        username: "",
+        password: ""
     }
     render() {
         return (<div id = "main">
-            <div id = "splash3pm"></div>
+            <div id = "splash3pm"><img src = "http://localhost:3000/3pmLogo2.png" style = {{ marginTop: "50px" }}></img></div>
             <div id = "inputPanel" style = {{ marginLeft: "auto", marginRight:"auto", marginTop:"50px" }}>
                 <label htmlFor="username">Username</label>
                 <tr></tr><input name="username"></input><br></br><br></br>
                 <label htmlFor="password">Password</label>
-                <tr></tr><input name="password"></input>
+                <tr></tr><input name="password" type = "password"></input><br></br>
+                <img src="http://localhost:3000/myob-logo.jpg" width="150"></img>
+                <img src="http://localhost:3000/zoho-creator.jpg" width="150"></img>
             </div>
         </div>);
     }
