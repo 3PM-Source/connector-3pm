eb76b875cf37e89edc1ba113338d4c28bdd21310
Asjad88 <asjad.amin@3pm.nz>
2021-08-19 12:37:17 +1200

    updated environment variables to suit Heroku postgreSQL related environment variable

--- services/api.js
+++ services/api.js
@@ -5,7 +5,6 @@ apiEnv.config();
 
 const myobApiUrl = "https://www.3pm.nz/myob";
 const db_url = process.env.DATABASE_URL;
-console.log(db_url);
 const essentialStr = db_url.split("://")[1];
 const dbUser = essentialStr.split(":")[0];
 const dbMasterPass = essentialStr.split(dbUser + ":")[1].split("@")[0];
@@ -14,7 +13,6 @@ const dbPort = essentialStr.split(dbUser + ":" + dbMasterPass + "@" + host + ":"
 const dbName = essentialStr.split(dbUser + ":" + dbMasterPass + "@" + host + ":" + dbPort + "/")[1];
 //const auth = new AUTHENTICATION(process.env.dbUser, process.env.host, process.env.dbName, process.env.dbMasterPass, process.env.dbPort);
 const auth = new AUTHENTICATION(dbUser, host, dbName, dbMasterPass, dbPort);
-console.log("AUTH", auth);
 const myob = new MYOB(process.env.clientId, process.env.clientSecret, process.env.redirectUrl, process.myobServerDomain);
 
 module.exports = { myobApiUrl, auth, myob };
