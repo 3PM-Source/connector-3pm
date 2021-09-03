0ecaa07604edf2acc54fff2f23934601372df10e
Asjad88 <asjad.amin@3pm.nz>
2021-09-03 14:55:44 +1200

    Added Logs for size of batchpayload

--- class/zoho.js
+++ class/zoho.js
@@ -347,10 +347,11 @@ class Zoho {
                             batchPayload[nestedArrayIndex][0]["data"].push(payload[x]);
                         }
                     }
-                console.log("BATCHPAYLOAD = ", batchPayload[0], "SIZE = ", batchPayload.length);
             } else {
                 batchPayload = [[{ data: payload, result: { fields: includeFields , tasks: includeTasks }}]];
             }
+            console.log("PAYLOAD = ", payload[0], "SIZE = ", payload.length);
+            console.log("BATCHPAYLOAD = ", batchPayload[0], "SIZE = ", batchPayload.length);
             const tokens = (await dbClient.getOAuth2Token("zoho_oauth2_tokens"))["oauth_token"]; //JSON.parse(await openFile(this.tokensPath));
             let start = performance.now();
             let created = [];
